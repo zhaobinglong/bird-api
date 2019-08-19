0a0f09f204bf3f37af41f0e269a81c9717a29bdc
赵冰龙 <binglong@from.mac>
2019-08-19 17:07:57 +0800

    add 公司资料信息

--- php-excel.php
+++ php-excel.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/PHPExcel-1.8/Classes/PHPExcel/IOFactory.php';
 include __DIR__ . '/core/lib/db.class.php';
 $db = new db();
 
-$filename = __DIR__ . '/doc/1.xlsx';
+$filename = __DIR__ . '/doc/2.xlsx';
 $objPHPExcelReader = PHPExcel_IOFactory::load($filename);
 
 $sheet = $objPHPExcelReader->getSheet(0); // 读取第一个工作表(编号从 0 开始)
@@ -17,13 +17,13 @@ $arr = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N
 $res_arr = array();
 for ($row = 2; $row <= $highestRow; $row++) {
 	$row_arr = array();
-	for ($column = 0; $arr[$column] != 'O'; $column++) {
+	for ($column = 0; $arr[$column] != 'E'; $column++) {
 		$val = $sheet->getCellByColumnAndRow($column, $row)->getValue();
 		$row_arr[] = $val;
 	}
-
-	$sql = "insert into bird_seller(`company_code`, `company_name`, `sub_company_code`, `sub_company_name`, `team_code`, `team_name`, `team_type`, `user_code`, `user_trans_code`, `user_name`, `user_param`, `user_type`, `user_post`, `user_office`) value('" . $row_arr[0] . "','" . $row_arr[1] . "','" . $row_arr[2] . "','" . $row_arr[3] . "','" . $row_arr[4] . "','" . $row_arr[5] . "','" . $row_arr[6] . "','" . $row_arr[7] . "','" . $row_arr[8] . "','" . $row_arr[9] . "','" . $row_arr[10] . "','" . $row_arr[11] . "','" . $row_arr[12] . "','" . $row_arr[13] . "')";
-	var_dump($sql);
-	$db->dql($sql);
-	$res_arr[] = $row_arr;
+	var_dump($row_arr);
+	// $sql = "insert into bird_seller(`company_code`, `company_name`, `sub_company_code`, `sub_company_name`, `team_code`, `team_name`, `team_type`, `user_code`, `user_trans_code`, `user_name`, `user_param`, `user_type`, `user_post`, `user_office`) value('" . $row_arr[0] . "','" . $row_arr[1] . "','" . $row_arr[2] . "','" . $row_arr[3] . "','" . $row_arr[4] . "','" . $row_arr[5] . "','" . $row_arr[6] . "','" . $row_arr[7] . "','" . $row_arr[8] . "','" . $row_arr[9] . "','" . $row_arr[10] . "','" . $row_arr[11] . "','" . $row_arr[12] . "','" . $row_arr[13] . "')";
+	// var_dump($sql);
+	// $db->dql($sql);
+	// $res_arr[] = $row_arr;
 }
