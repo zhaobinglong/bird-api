f636f5fcdc263d20578f3d80c9cde0b8dd55383f
赵冰龙 <binglong@from.mac>
2019-08-20 14:15:56 +0800

    add 增加第三份表格

--- php-excel.php
+++ php-excel.php
@@ -17,13 +17,16 @@ $arr = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N
 $res_arr = array();
 for ($row = 2; $row <= $highestRow; $row++) {
 	$row_arr = array();
-	for ($column = 0; $arr[$column] != 'E'; $column++) {
+	for ($column = 0; $arr[$column] != 'M'; $column++) {
 		$val = $sheet->getCellByColumnAndRow($column, $row)->getValue();
 		$row_arr[] = $val;
 	}
+	var_dump($row_arr);
+	// 插入HR销售人员
+	// $sql = "insert into bird_seller(`user_name`,`user_code`,`company_code`, `company_name`, `sub_company_code`, `sub_company_name`, `team_code`, `team_name`, `identify_number`,  `bank_code`, `bank_name`, `phone_number`, `user_classify`) value('" . $row_arr[1] . "','" . $row_arr[2] . "','" . $row_arr[3] . "','" . $row_arr[4] . "','" . $row_arr[5] . "','" . $row_arr[6] . "','" . $row_arr[7] . "','" . $row_arr[8] . "','" . $row_arr[9] . "','" . $row_arr[10] . "','" . $row_arr[11] . "','" . $row_arr[12] . "')";
 
-	$sql = "insert into bird_company(`company_code`, `company_name`, `sub_company_code`, `sub_company_name`) value('" . $row_arr[0] . "','" . $row_arr[1] . "','" . $row_arr[2] . "','" . $row_arr[3] . "')";
+	// $sql = "insert into bird_company(``user_name`, `user_code`,`company_code`, `company_name`, `sub_company_code`, `sub_company_name`,``) value('" . $row_arr[0] . "','" . $row_arr[1] . "','" . $row_arr[2] . "','" . $row_arr[3] . "')";
 	// var_dump($sql);
-	$db->dql($sql);
+	// $db->dql($sql);
 	$res_arr[] = $row_arr;
 }
