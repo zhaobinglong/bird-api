669cf467c04096758aa8e84e61f40e110bafc498
赵冰龙 <binglong@from.mac>
2019-08-22 11:51:48 +0800

    up 增加action

--- php-excel.php
+++ php-excel.php
@@ -7,6 +7,7 @@ $db = new db();
 
 $excel = $_GET['page'];
 $sheet = $_GET['sheet'];
+$action = $_GET['action'];
 
 $filename = __DIR__ . '/doc/' . $excel . '.xlsx';
 $objPHPExcelReader = PHPExcel_IOFactory::load($filename);
@@ -33,24 +34,29 @@ for ($row = 5; $row <= $highestRow; $row++) {
 	if (empty($row_arr[1])) {
 		continue;
 	}
-	// print_r($row_arr);
-	// echo "<br/>";
 
-	// 插入HR销售人员
-	// $sql = "insert into bird_seller(`user_name`,`user_code`,`company_code`, `company_name`, `sub_company_code`, `sub_company_name`, `team_code`, `team_name`, `identify_number`,  `bank_code`, `bank_name`, `phone_number`, `user_classify`) value('" . $row_arr[1] . "','" . $row_arr[2] . "','" . $row_arr[3] . "','" . $row_arr[4] . "','" . $row_arr[5] . "','" . $row_arr[6] . "','" . $row_arr[7] . "','" . $row_arr[8] . "','" . $row_arr[9] . "','" . $row_arr[10] . "','" . $row_arr[11] . "','" . $row_arr[12] . "', 'HR销售人员')";
-	// $res = $db->dql($sql);
-	// var_dump($res);
-
-	// 插入非HR销售人员
-	// $sql = "insert into bird_seller(`user_code`,`user_name`, `identify_number`, `bank_code`, `bank_name`, `phone_number`,`user_classify`) value('" . $row_arr[2] . "','" . $row_arr[11] . "','" . $row_arr[12] . "','" . $row_arr[13] . "','" . $row_arr[14] . "','" . $row_arr[15] . "','非HR销售人员')";
-	// $res = $db->dql($sql);
-	// var_dump($res);
-	// echo "<br/>";
-
-	// 通过手机号码更改销售人员的归属代码
-	$sql = "update bird_seller set team_code='" . $row_arr[7] . "' where phone_number='" . $row_arr[15] . "'";
-	$res = $db->dql($sql);
-	var_dump($res);
-	echo "<br/>";
+	if ($action == '1') {
+		print_r($row_arr);
+		echo "<br/>";
+	} elseif ($action == '2') {
+		// 插入HR销售人员
+		$sql = "insert into bird_seller(`user_name`,`user_code`,`company_code`, `company_name`, `sub_company_code`, `sub_company_name`, `team_code`, `team_name`, `identify_number`,  `bank_code`, `bank_name`, `phone_number`, `user_classify`) value('" . $row_arr[1] . "','" . $row_arr[2] . "','" . $row_arr[3] . "','" . $row_arr[4] . "','" . $row_arr[5] . "','" . $row_arr[6] . "','" . $row_arr[7] . "','" . $row_arr[8] . "','" . $row_arr[9] . "','" . $row_arr[10] . "','" . $row_arr[11] . "','" . $row_arr[12] . "', 'HR销售人员')";
+		$res = $db->dql($sql);
+		var_dump($res);
+	} elseif ($action == '3') {
+		// 插入非HR销售人员
+		$sql = "insert into bird_seller(`user_code`,`user_name`, `identify_number`, `bank_code`, `bank_name`, `phone_number`,`user_classify`) value('" . $row_arr[2] . "','" . $row_arr[11] . "','" . $row_arr[12] . "','" . $row_arr[13] . "','" . $row_arr[14] . "','" . $row_arr[15] . "','非HR销售人员')";
+		$res = $db->dql($sql);
+		var_dump($res);
+		echo "<br/>";
+	} elseif ($action == '4') {
+		// 通过手机号码更改销售人员的归属代码
+		$sql = "update bird_seller set team_code='" . $row_arr[7] . "' where phone_number='" . $row_arr[15] . "'";
+		$res = $db->dql($sql);
+		var_dump($res);
+		echo "<br/>";
+	} else {
+		echo '未知的action';
+	}
 	// $res_arr[] = $row_arr;
 }
